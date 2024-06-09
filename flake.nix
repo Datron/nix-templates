9b714fcb6724e5ddf1eda2e16e9f62f11179ce52
datron <kartik1702@gmail.com>
2024-06-09 09:30:48 +0530

    initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+# in flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            # 👇 we can just use `rustToolchain` here:
+            packages = with pkgs; [
+              cargo-watch
+              nodePackages.wrangler
+              rustup
+              nodejs_18
+            ];
+          };
+        }
+      );
+}
